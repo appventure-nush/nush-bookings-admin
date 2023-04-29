94ae0e83f101f401afc025c552c3f0963c9ccd8c
iamdedeepsai <dedeepsai.vadapalli@gmail.com>
2023-04-29 11:12:09 +0800

    implemented manage bookings page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ManageBookings />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ManageBookings from "@/components/ManageBookings.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ManageBookings
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    background: #1c1c1c;
+  }
+
+  @font-face {
+      font-family: 'Avenir';
+      font-style: normal;
+      font-weight: 400;
+      src: url('./assets/fonts/AvenirLTStd-Roman.otf') format('opentype');
+  }
+  @font-face {
+      font-family: 'Avenir';
+      font-style: normal;
+      font-weight: 500;
+      src: url('./assets/fonts/AvenirLTStd-Medium.otf') format('opentype');
+  }
+
+  @font-face {
+      font-family: 'Avenir';
+      font-style: normal;
+      font-weight: 700;
+      src: url('./assets/fonts/AvenirLTStd-Heavy.otf') format('opentype');
+  }
+
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Avenir';
+      /* line-height: 1.2; */
+      -webkit-tap-highlight-color: transparent;
+  }
 </style>
